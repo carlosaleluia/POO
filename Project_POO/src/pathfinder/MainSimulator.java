7eeb3384bdec9d629339fdae1f6cc775e73b1144
Filipa <Filipa@194.210.234.36>
2018-05-02 11:33:52 +0100

    teste123

--- Project_POO/src/pathfinder/MainSimulator.java
+++ Project_POO/src/pathfinder/MainSimulator.java
@@ -1,14 +1,17 @@
 package pathfinder;
 
-import java.util.LinkedList;
+import java.util.LinkedList; //??
 import simulator.EventSimulator;
 
 public class MainSimulator extends EventSimulator{
 	
 	
-	final int max_population, comfortsens, n , m;
-	
-	final Point initial, destination;
+	final int init_population, max_population;
+	final int n, m; //n is the number of lines, m is the number of columns
+	final int comfortsens; // Comfort Parameter
+	final int finalinst; // Final instant of the simulation
+	final int param_death, param_move, param_reproduce;	//Parameteres for number generator
+	final Point initial, destination; // Initial and Final Point
 	double[] death_param, move_param, reproduction_param;
 	
 	boolean final_point_hit;
@@ -20,23 +23,25 @@ public class MainSimulator extends EventSimulator{
 	
 	Individual the_best;
 	
-	public MainSimulator(String s) {
-				
+	public MainSimulator(String s) {				
 		ReadFile r = new ReadFile(s); 
 		r.readXML();		
 		
 		Map = new Grid(r);
 		Map.initializeGrid();
 		
-		this.max_population = r.maxpop;
-		this.comfortsens = r.comfortsens;
+		this.init_population = r.initpop;
+		this.max_population = r.maxpop;				
 		this.n = r.rowsnb;
 		this.m= r.colsnb;
-		
+		this.comfortsens = r.comfortsens;			
 		this.initial = r.initialpoint;
-		this.destination = r.finalpoint;
-		
-		this.list_individuals = new LinkedList<Individual>();	
+		this.destination = r.finalpoint;		
+		this.finalinst = r.finalinst;
+		this.param_death = r.param_death;
+		this.param_move = r.param_move;
+		this.param_reproduce = r.param_reproduce;		
+		this.list_individuals = new LinkedList<Individual>();
 
 	}
 	
