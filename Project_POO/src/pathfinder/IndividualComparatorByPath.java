3c74957305f1ad35c7de96f2691f38e53f10bb15
carlosaleluia <32376486+carlosaleluia@users.noreply.github.com>
2018-05-03 17:24:40 +0100

    comparepath

--- Project_POO/src/pathfinder/IndividualComparatorByPath.java
+++ Project_POO/src/pathfinder/IndividualComparatorByPath.java
@@ -10,12 +10,15 @@ public class IndividualComparatorByPath implements Comparator<Individual>{
 			return 1;
 		else if(i1.has_reached==false && i2.has_reached==true)
 			return -1;
-		else {
+		else if(i1.has_reached==true && i2.has_reached==true){
 			if (i1.current_cost < i2.current_cost) return 1;
 	        if (i1.current_cost > i2.current_cost) return -1;
-	        else
-	        	return 0;
 		}
+		else if(i1.has_reached==false && i2.has_reached==false) {
+			IndividualComparatorByComfort c=new IndividualComparatorByComfort();
+			return c.compare(i1, i2)*(-1);
+		}
+		return 0;
 	}
 
 }
