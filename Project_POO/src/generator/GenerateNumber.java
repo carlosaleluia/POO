b779516d65277d6c36c9d20755f7ce0775f7b912
Magali <magali_correia_96@hotmail.com>
2018-05-03 14:39:33 +0100

    generate

--- Project_POO/src/generator/GenerateNumber.java
+++ Project_POO/src/generator/GenerateNumber.java
@@ -1,15 +1,8 @@
 package generator;
 
-import java.util.Random;
-
 public interface GenerateNumber {
-
-	static Random random = new Random();
 	
-	public static double Generate(double[] par) {
-		double next =random.nextDouble();
-		return -par[0]*Math.log(1.0-next);
-	}
+	public double Generate(double[] par);
 }
 
 
